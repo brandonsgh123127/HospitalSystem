a3e7d86a7747227c4737c75472dbb3ee8ac15c86
Brandon Spada <brandonsgh123127@gmail.com>
2020-03-18 15:32:31 -0400

    Initialize Repository

---
+++ src/main/java/com/hospital/system/maven_hospital_system/App.java
@@ -0,0 +1,43 @@
+package com.hospital.system.maven_hospital_system;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+
+        stage.show();
+    }
+
+}
---
+++ src/test/java/com/hospital/system/maven_hospital_system/AppTest.java
@@ -0,0 +1,20 @@
+package com.hospital.system.maven_hospital_system;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
