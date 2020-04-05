ee1c5b159557714effbbf6edecc95c7926b6a082
Brandon Spada <brandonsgh123127@gmail.com>
2020-04-05 14:06:36 -0400

    Add sample queries for MySQL

---
+++ db/MYSQL_SAMPLE_INSERT_SCRIPT.sql
@@ -0,0 +1,6 @@
+/* SAMPLE OF HOW TO USE MYSQL TO INSERT INFO*/
+show databases;
+use mydb;
+show tables;
+insert into sample(name) values ('smith');
+select * from sample;
--- src/main/java/com/hospital/system/maven_hospital_system/App.java
+++ src/main/java/com/hospital/system/maven_hospital_system/App.java
@@ -22,23 +22,6 @@ public class App extends Application
     }
     @Override
     public void start(Stage stage) throws Exception {
-//        stage.setTitle("Hello World!");
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-// 
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        stage.setScene(new Scene(root, 300, 250));
-//
-//        stage.show();
-    	
     	//Start with login...
     	FXMLLoginController f = new FXMLLoginController(stage);
     	stage.setScene(f.LoginScreen());
