44d6f3785d59f30bc36eb56ba511da12d4ff2737
Brandon Spada <brandonsgh123127@gmail.com>
2020-04-11 13:21:53 -0400

    Add more FXML files

--- src/main/java/com/hospital/system/maven_hospital_system/AdministratorController.java
+++ src/main/java/com/hospital/system/maven_hospital_system/AdministratorController.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.sql.*;
+
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,13 +14,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumnBase;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class AdministratorController {
+public class AdministratorController extends Thread{
 
+	private String identification;
 	@FXML
 	private TableView<Event> table;
 	@FXML
@@ -26,13 +32,24 @@ public class AdministratorController {
 	private CheckBox addCheck,removeCheck;
 	@FXML
 	private Button submitButton;
-	
+	@FXML
 	private Stage stage;
-	private Scene scene;
+	@FXML
+	private Label label;
+	@FXML
+	private ObservableList<TableColumn<Event, ?>> tableContents;
 	
-	public AdministratorController(Stage stage, Scene scene) {
+	private Connection con;
+	//private Scene scene;
+	
+	public AdministratorController(Stage stage, Scene scene,Connection con,String identification) {
 		this.stage = stage;
-		this.scene=scene;
+		this.con=con;
+		try{fillDB();}
+		catch(Exception e) {
+			System.out.println("FAILED TO FILL DATABASE WITH INFO!!");
+		}
+		//this.scene=scene;
 	}
 	
 	public Scene displayPage() throws IOException {
@@ -42,11 +59,31 @@ public class AdministratorController {
 		FileInputStream fxmlStream = new FileInputStream(path);
 		//Load the FXML file
 		Parent root = loader.load(fxmlStream);
-		this.scene = new Scene(root);
-		table = (TableView<Event>) scene.lookup("#table");
+		//this.scene = new Scene(root);
 		//role = (TableColumn<Event,String>)scene.lookup("#role");
+        Scene scene = new Scene(root);
+		table = (TableView<Event>) scene.lookup("#table");
+		//lastName = (TableColumn<Event,?> scene.lookup("#lastName"));
+//		tableContents = table.getColumns();
+//		lastName =(TableColumn<Event,String>) tableContents.get(3);
+//		firstName =(TableColumn<Event,String>) tableContents.get(2);
+//		id =(TableColumn<Event,String>) tableContents.get(0);
+//		role =(TableColumn<Event,String>) tableContents.get(1);		
+		stage.setScene(scene);
+        stage.show();
 		return scene;
+	}
+	
+	public void fillDB() throws SQLException {
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("SELECT * FROM users"); 
+		while(rs.next()) {
+			table.getItems().addAll();
 
+		}
+
+
+		
 	}
 	
 }
